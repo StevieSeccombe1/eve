3e58956a950a73cbaf7f0dc23787558a7b5434ee
kerriebaggs <103642998+kerriebaggs@users.noreply.github.com>
2022-06-01 10:39:44 +0100

    Update AboutUsSection.js

--- pages-sections/AboutUsSection.js
+++ pages-sections/AboutUsSection.js
@@ -19,11 +19,11 @@ export default function AboutUsSection() {
 Located in the heart of Exeter in the South West, EVE offers air quality consultancy to a range of sectors.
                         </h5>
                         <h5 className={classes.description}>
-Kerrie, the founder of Exe Valley Environmental, brings passion, skill and a friendly attitude to the consultancy.
-She has over seven years experience in the industry working on a range of projects, from major infrastructure projects and large-scale mixed-use developments to early feasibility assessments for residential developments and indoor air quality surveys.
+Kerrie, the founder of Exe Valley Environmental, brings passion, skill and a friendly attitude to consultancy.
+She has over seven years' experience in the industry working on a range of projects, from major infrastructure and large-scale mixed-use developments to early feasibility assessments for residential developments and indoor air quality surveys.
                         </h5>
                         <h5 className={classes.description}>
-Kerrie is a member of the Institute of Air Quality Management (IAQM), a member of the Institute of Environmental Sciences (IES) and a practitioner of the Institute of Environmental Management & Assessment (IEMA).
+Kerrie is a member of the Institute of Air Quality Management, a member of the Institute of Environmental Sciences and a practitioner of the Institute of Environmental Management & Assessment.
                         </h5>
                     </GridItem>
                     <img className={classes.circleBox} width="200px" src="https://media-exp1.licdn.com/dms/image/C4D03AQFzkL_au1UoSA/profile-displayphoto-shrink_400_400/0/1617888199178?e=1655942400&v=beta&t=H_g34iiL3x6c5Q2ODGNAUKcr9EC3GfYEZIWkQ9O_vyg"/></GridContainer>
